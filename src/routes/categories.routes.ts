dd9b6e713015bd6b7a4cfbabba27fbff7ec4a444
Felipe Fogaça <felipe.foogaca@gmail.com>
2021-06-16 23:49:23 -0300

    added: Category route, model and repository

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,18 +1,26 @@
 import { Router } from "express";
 
+import { CategoriesRepository } from "../repositories/CategoriesRepository";
+
 const categoriesRoutes = Router();
+const categoriesRepository = new CategoriesRepository();
 
-const categories = [];
-
-categoriesRoutes.post("/categories", (request, response) => {
+categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
 
-  categories.push({
-    name,
-    description,
-  });
+  const categoryAlredyExists = categoriesRepository.findByName(name);
+
+  if (categoryAlredyExists)
+    return response.status(400).json({ error: "Category alredy exists!" });
+
+  categoriesRepository.create({ name, description });
 
   return response.status(201).send();
+});
+
+categoriesRoutes.get("/", (request, response) => {
+  const all = categoriesRepository.list();
+  return response.json(all);
 });
 
 export { categoriesRoutes };
