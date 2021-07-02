76fb953aab9ae29d0b75c841301bea1c2b3d6cef
Felipe Fogaça <felipe.foogaca@gmail.com>
2021-07-01 21:38:19 -0300

    added: tsyringe dependence injection

--- src/modules/cars/repositories/implementations/CategoriesRepository.ts
+++ src/modules/cars/repositories/implementations/CategoriesRepository.ts
@@ -11,8 +11,6 @@ import {
 class CategoriesRepository implements ICategoriesRepository {
   private repository: Repository<Category>;
 
-  private static INSTANCE: CategoriesRepository;
-
   constructor() {
     this.repository = getRepository(Category);
   }
