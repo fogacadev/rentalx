bc018f08730cb356d71e1eaacec2790e2481c636
Felipe Fogaça <felipe.foogaca@gmail.com>
2021-07-20 21:51:25 -0300

    added seed:admin fro create admin user

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -2,15 +2,15 @@ import express, { NextFunction, Request, Response } from "express";
 import "express-async-errors";
 import swaggerUi from "swagger-ui-express";
 
-import "../typeorm";
-
 import "@shared/container";
 
 import { AppError } from "@shared/errors/AppError";
 import { router } from "@shared/infra/http/routes";
+import createConnection from "@shared/infra/typeorm/index";
 
 import swaggerFile from "../../../swagger.json";
 
+createConnection();
 const app = express();
 app.use(express.json());
 
