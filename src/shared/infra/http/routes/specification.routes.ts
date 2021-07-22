a2650ff7e9a67227f2c80697f807983ed0daef9f
Felipe Fogaça <felipe.foogaca@gmail.com>
2021-07-22 20:59:37 -0300

    added: ensureamin middleware to routes

--- src/shared/infra/http/routes/specification.routes.ts
+++ src/shared/infra/http/routes/specification.routes.ts
@@ -2,13 +2,17 @@ import { Router } from "express";
 
 import { CreateSpecificationController } from "@modules/cars/useCases/createSpecification/CreateSpecificationController";
 
+import { ensureAdmin } from "../middlewares/ensureAdmin";
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 
 const specificationRoutes = Router();
 const createdSpecificationController = new CreateSpecificationController();
 
-specificationRoutes.use(ensureAuthenticated);
-
-specificationRoutes.post("/", createdSpecificationController.handle);
+specificationRoutes.post(
+  "/",
+  ensureAuthenticated,
+  ensureAdmin,
+  createdSpecificationController.handle
+);
 
 export { specificationRoutes };
